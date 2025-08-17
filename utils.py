23a86f2e586f641d08a07b36766efd5ab2a1b51b
adem667 <110057698+adem667@users.noreply.github.com>
2025-08-17 18:15:30 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,12 +1,13 @@
+
 from datetime import datetime
 
-def is_valid_key(provided, real):
-    return provided == real
+def is_valid_key(key, correct_key):
+    return key == correct_key
 
 def parse_expiration_date(date_str):
     try:
         return datetime.strptime(date_str, "%Y-%m-%d")
-    except Exception:
+    except:
         return None
 
 def is_account_expired(account):
